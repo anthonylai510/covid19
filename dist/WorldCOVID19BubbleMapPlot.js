f00613cdb0ef288d0ff04a078fe10decda4bc8c9
anthonylai510 <anthonylai510@gmail.com>
2020-03-19 19:42:22 -0700

    Update by adding a jexcel table below chart
    Update by adding a jexcel table below chart

--- dist/WorldCOVID19BubbleMapPlot.js
+++ dist/WorldCOVID19BubbleMapPlot.js
@@ -1,52 +1,35 @@
-function getOffsetDate(offset, format) {
-	var tday;
-	var today = new Date();
-
-	today.setDate(today.getDate() - offset);
-	var dd = today.getDate();
-
-	var mm = today.getMonth()+1; 
-	var yyyy = today.getFullYear();
-
-	if(dd<10) 
-	{
-		dd='0'+dd;
-	} 
-
-	if(mm<10) 
-	{
-		mm='0'+mm;
-	}
-	
-	if(format == "yyyy-mm-dd")
-		tday = yyyy + '-' + mm + '-' + dd;
-	else if (format == "mm-dd-yyyy")
-		tday = mm + '-' + dd + '-' + yyyy;
-	else
-		tday = yyyy + '-' + mm + '-' + dd;
-
-	return tday;
-}
-
-
 //get the date string for today
-var tday = getOffsetDate(0, "mm-dd-yyyy");
-console.log(tday);
+var strdate = getOffsetDate(0, "mm-dd-yyyy");
+//console.log(strdate);
 
+var valuesGroupByColumn;
+const groupByColumn = "Country/Region";
+const aggType = "sum";
+
+//download csv source data and then make scattergeo plot and generate jexcel table
 Plotly.d3.csv(
-	"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + tday + ".csv",
+	"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + strdate + ".csv",
 	function(err, rows) {
 		if(err == null)
-			makePlot(err, rows, tday);
+		{
+			console.log(strdate);
+			strdate = getOffsetDate(0, "yyyy-mm-dd"); //change the date format to "yyyy-mm-dd" for displaying on the heading of the scattergeo chart
+			makePlot(err, rows, strdate);
+			groupByData(rows, groupByColumn, aggType)
+			makeTable(rows);
+		}
 		else
 		{
 			//get the date string for yesterday
-			tday = getOffsetDate(1, "mm-dd-yyyy");
-			console.log(tday);
+			strdate = getOffsetDate(1, "mm-dd-yyyy");
+			console.log(strdate);
 			Plotly.d3.csv(
-				"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + tday + ".csv",
+				"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + strdate + ".csv",
 				function(err, rows) {
-					makePlot(err, rows, tday);
+					strdate = getOffsetDate(1, "yyyy-mm-dd"); //change the date format to "yyyy-mm-dd" for displaying on the heading of the scattergeo chart
+					makePlot(err, rows, strdate);
+					groupByData(rows, groupByColumn, aggType)
+					makeTable(rows);
 				}
 			);
 		}
@@ -54,9 +37,10 @@ Plotly.d3.csv(
 );
 
 
-function makePlot(err, rows, tday)
+//function for create the plotly scattergeo plot
+function makePlot(err, rows, strdate)
 {
-
+		console.log("Before create COVID-19 World Map");
 		//console.log(err);
 		//console.log(rows);
 
@@ -71,18 +55,15 @@ function makePlot(err, rows, tday)
 		  return total + Math.round(num);
 		}
 
-		var scl1 = [[0.7,'rgb(102, 0, 0)'],[1,'rgb(90, 0, 0)']];
-		var scl2 = [[0.5,'rgb(204, 0, 0)'], [0.6,'rgb(135, 0, 0)']];
-		var scl3 = [[0,'rgb(255, 51, 51)'],[0.35,'rgb(255, 0, 0)']];
-		var scl4 = [[0,'rgb(255, 51, 51)'],[0.35,'rgb(255, 0, 0)'],[0.5,'rgb(204, 0, 0)'], [0.6,'rgb(135, 0, 0)'],[0.7,'rgb(102, 0, 0)'],[1,'rgb(90, 0, 0)']];
-		
+
 		
 		var cityName = unpack(rows, 'Province/State'),
 			countryName = unpack(rows, 'Country/Region'),
 			cityCases = unpack(rows, 'Confirmed'),
+			cityDeaths = unpack(rows, 'Deaths'),
+			cityRecovered = unpack(rows, 'Recovered'),
 			cityLat = unpack(rows, 'Latitude'),
 			cityLon = unpack(rows, 'Longitude'),
-			color = [,"rgb(255,65,54)","rgb(133,20,75)","rgb(255,133,27)","lightgrey"],
 			hoverText = [];
 		
 		//calculate the Total Confirmed Cases
@@ -93,12 +74,13 @@ function makePlot(err, rows, tday)
 		for ( var i = 0 ; i < cityCases.length; i++) {
 			var currentText = "";
 			if(cityName[i] == "")
-				currentText = countryName[i] + "<br>Cases: " + cityCases[i];
+				currentText = countryName[i] + "<br>Confirmed: " + cityCases[i] + "<br>Deaths: " + cityDeaths[i] + "<br>Recovered: " + cityRecovered[i];
 			else
-				currentText = cityName[i] + ", " + countryName[i] + "<br>Cases: " + cityCases[i];
+				currentText = cityName[i] + ", " + countryName[i] + "<br>Confirmed: " + cityCases[i] + "<br>Deaths: " + cityDeaths[i] + "<br>Recovered: " + cityRecovered[i];
 			hoverText.push(currentText);
 		}
 
+		//trace1: Confirmed Cases > 10,000
 		var trace1 = 
 			{
 				name: 'Confirmed > 10,000',
@@ -134,7 +116,7 @@ function makePlot(err, rows, tday)
 				]
 			};
 
-
+		//trace2: 1,000 < Confirmed Cases <= 10,000
 		var trace2 = 
 			{
 				name: '1,000 < Confirmed <= 10,000',
@@ -175,7 +157,7 @@ function makePlot(err, rows, tday)
 				]
 			};
 
-
+		//trace3: 100 < Confirmed Cases <= 1,000
 		var trace3 = 
 			{
 				name: '100 < Confirmed <= 1,000',
@@ -216,7 +198,7 @@ function makePlot(err, rows, tday)
 				]
 			};
 
-
+		//trace4: 1 <= Confirmed Cases <= 100
 		var trace4 = 
 			{
 				name: '1 < Confirmed <= 100',
@@ -241,7 +223,7 @@ function makePlot(err, rows, tday)
 					autocolorscale: false,
 					size: unpack(rows, 'Confirmed'),
 					sizemode: "area",
-					sizeref: 0.6 // size ref for 0 <= value <= 100
+					sizeref: 0.6// size ref for 0 <= value <= 100
 				},
 				transforms: [
 				  {	type: 'filter',
@@ -293,8 +275,9 @@ function makePlot(err, rows, tday)
 			orientation: "h"
 		  },
 		  title: {
-					//text: '<b>COVID-19 Global Confirmed Cases on ' + today + ' (by Anthony Lai)<br>Total Confirmed: ' + arrcityCasesSum + '</b>', 
-					text: '<b>Total Confirmed: ' + arrcityCasesSum.toLocaleString('en-US') + '</b>', 
+					//text: '<b><font size="5">COVID-19 Global Confirmed Cases on ' + strdate + ' (by Anthony Lai)</font></b><br>' + '<b>Total Confirmed: ' + arrcityCasesSum.toLocaleString('en-US') + '</b>', 
+					text: '<b>COVID-19 Global Confirmed Cases on ' + strdate + ' (by Anthony Lai)</b><br>' + '<b>Total Confirmed: ' + arrcityCasesSum.toLocaleString('en-US') + '</b>', 
+					
 					font: {
 					  family: 'Courier New, monospace',
 					  size: 22,
@@ -309,10 +292,10 @@ function makePlot(err, rows, tday)
 
 		};
 		
-		var h2 = document.getElementById('myh2');
+		//var h2 = document.getElementById('myh2');
 		
-		h2.innerHTML  = '<b><font size="5">COVID-19 Global Confirmed Cases on ' + tday + ' (by Anthony Lai)</font></b>';
-		h2.align = 'center';
+		//h2.innerHTML  = '<b><font size="5">COVID-19 Global Confirmed Cases on ' + strdate + ' (by Anthony Lai)</font></b>';
+		//h2.align = 'center';
 		
 		var config = {responsive: true, displayModeBar: false}; //hide the plotly menubar
 		
@@ -320,3 +303,101 @@ function makePlot(err, rows, tday)
 
 		Plotly.newPlot("myDiv", data, layout, config);
 };
+
+
+//function to sum(Confirmed), sum(Deaths), sum(Recovered) with group by Country and sorted by sum(Confirmed)
+function groupByData(data, groupByColumn, aggType)
+{
+	data.forEach(function(d){
+		//group and organize the data as needed here
+	  valuesGroupByColumn = d3.nest()
+	  //set the groupByColumn as the key
+	  .key(function(d) {return d[groupByColumn];}) 
+	  //rollup and sum the cases values by groupByColumn
+	  .rollup((function(d) {
+		return {
+			Confirmed: d3.sum(d, function(e) { return e["Confirmed"]; }),
+			Deaths: d3.sum(d, function(e) { return e["Deaths"]; }),
+			Recovered: d3.sum(d, function(e) { return e["Recovered"]; }),
+		};
+	  }))
+	  .entries(data);
+	});
+	
+	//Re-arrange GroupBy Data
+	var newData = [];
+	var rows = valuesGroupByColumn;
+	for (var key in rows) { 
+		if (rows.hasOwnProperty(key) && rows[key].value['Confirmed'] >= 1) {
+			var tmp = {}
+			//var tmp = rows[key].value;
+			tmp["Country"] = rows[key].key;
+			tmp["Confirmed"] = rows[key].value['Confirmed'];
+			tmp["Deaths"] = rows[key].value['Deaths'];
+			tmp["Recovered"] = rows[key].value['Recovered'];
+			console.log(tmp)
+			newData.push(tmp);
+		} 
+	}
+	
+	//Sort the data by descending
+	newData = newData.sort(function(a, b) {
+		return d3['descending'](a.Confirmed, b.Confirmed);
+		//return d3['descending'](a.Deaths, b.Deaths);
+		//return d3['descending'](a.Recovered, b.Recovered);
+	});
+	
+	//assign the new data to valuesGroupByColumn
+	valuesGroupByColumn = newData;
+}
+
+//function to create the jexcel table sorted by Confirmed Cases descending
+function makeTable(rows)
+{
+	console.log("Before create jexcel table");
+	console.log(valuesGroupByColumn);
+	jexcel(document.getElementById('myTable'), {
+		data: valuesGroupByColumn,
+		csvHeaders: true,
+		search: true,
+		pagination: 15,
+		columns: [
+			{ type:'text', width:250, title:'Country'},
+			{ type:'text', width:150, title:'Confirmed'},
+			{ type:'text', width:150, title:'Deaths'},
+			{ type:'text', width:150, title:'Recovered'},
+		 ]
+	});
+}
+
+
+//function to get date string with offset and format
+function getOffsetDate(offset, format) {
+	var tday;
+	var today = new Date();
+
+	today.setDate(today.getDate() - offset);
+	var dd = today.getDate();
+
+	var mm = today.getMonth()+1; 
+	var yyyy = today.getFullYear();
+
+	if(dd<10) 
+	{
+		dd='0'+dd;
+	} 
+
+	if(mm<10) 
+	{
+		mm='0'+mm;
+	}
+	
+	if(format == "yyyy-mm-dd")
+		tday = yyyy + '-' + mm + '-' + dd;
+	else if (format == "mm-dd-yyyy")
+		tday = mm + '-' + dd + '-' + yyyy;
+	else
+		tday = yyyy + '-' + mm + '-' + dd;
+
+	return tday;
+}
